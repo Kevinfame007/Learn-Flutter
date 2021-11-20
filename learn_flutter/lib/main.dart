4702e216808fad6731b2f4c31fe0c61412e02882
Kevinfame007 <kevinfame007@gmail.com>
2021-11-20 15:05:59 +0700

    learn how to use Stateful widget
    learn how to use Stateful widget

--- learn_flutter/lib/main.dart
+++ learn_flutter/lib/main.dart
@@ -12,9 +12,40 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: "My App",
-      home: Scaffold(
+      home: MyHomePage(),
+      theme: ThemeData(primarySwatch: Colors.lightGreen),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({ Key? key }) : super(key: key);
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(vsync: this);
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _controller.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
         appBar: AppBar(
-          title: Text("mazuma Attendance Time App"),
+          title: Text("mazuma"),
         ),
         body: Center(
           child: Column(
@@ -26,8 +57,6 @@ class MyApp extends StatelessWidget {
             ],
           ),
         ),
-      ),
-      theme: ThemeData(primarySwatch: Colors.lightGreen),
     );
   }
 }
